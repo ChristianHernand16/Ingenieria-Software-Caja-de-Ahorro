97b15238bc8173a449f73315504369b19d276a96
ChristianHernand16 <49212986+ChristianHernand16@users.noreply.github.com>
2019-12-18 09:25:14 -0500

    octavo cambio

--- src/ec/edu/ups/controlador/ControladorSecretaria.java
+++ src/ec/edu/ups/controlador/ControladorSecretaria.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package ec.edu.ups.controlador;
-
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
